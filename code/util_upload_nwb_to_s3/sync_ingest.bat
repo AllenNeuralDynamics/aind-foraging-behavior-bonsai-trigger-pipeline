7bd612bc2714069f28d85ef9aa12ac3c02cfad8f
Han Hou <houhan@gmail.com>
2025-03-05 14:37:03 -0800

    back to my PC

--- code/util_upload_nwb_to_s3/sync_ingest.bat
+++ code/util_upload_nwb_to_s3/sync_ingest.bat
@@ -3,6 +3,5 @@ call :LOG >> %LOGFILE% 2>&1
 exit /B
 
 :LOG
-call "C:\Users\svc_aind_behavior\AppData\Local\miniconda3\Scripts\activate.bat" han_temp_pipeline
-python "C:\Users\svc_aind_behavior\Documents\aind-foraging-behavior-bonsai-trigger-pipeline\code\util_upload_nwb_to_s3\behavior_pipeline_bonsai.py"
-net use z: \\allen\aind\scratch /persistent:Yes
+call "C:\Users\han.hou\AppData\Local\miniconda3\condabin\activate.bat" han_temp_pipeline
+python "E:\Scripts\aind-foraging-behavior-bonsai-trigger-pipeline\code\util_upload_nwb_to_s3\behavior_pipeline_bonsai.py"
