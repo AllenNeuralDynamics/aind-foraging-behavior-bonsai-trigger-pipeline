e6a31ea340da3e695c23906aa3d8e257f5c248f7
Han Hou <houhan@gmail.com>
2025-05-20 14:09:54 -0700

    fix: update rig mapping for two old PCs for 447-3

--- code/util_upload_nwb_to_s3/sync_ingest_training_rig.bat
+++ code/util_upload_nwb_to_s3/sync_ingest_training_rig.bat
@@ -6,3 +6,4 @@ exit /B
 call "C:\Users\svc_aind_behavior\AppData\Local\miniconda3\Scripts\activate.bat" han_temp_pipeline
 python "C:\Users\svc_aind_behavior\Documents\aind-foraging-behavior-bonsai-trigger-pipeline\code\util_upload_nwb_to_s3\behavior_pipeline_bonsai.py"
 net use z: \\allen\aind\scratch /persistent:Yes
+
